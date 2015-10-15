85dc66179ae857e6972f8788e4cf96073959ccbb
User1 <charlie_d_s@hotmail.com>
2015-10-15 17:05:28 +0200

    Got a basic version of learning working

--- brain/level.c
+++ brain/level.c
@@ -24,6 +24,7 @@ level *level_makeNew(int firstInputs, int outputs, int connections, level *previ
  newLvl->conOut = (unsigned char *)malloc(sizeof(unsigned char)*connections);
  newLvl->conWeight = (float *)malloc(sizeof(float)*connections);
  newLvl->outputsPreSigmoid = (int *)malloc(sizeof(int)*outputs);
+ newLvl->outputsIdealPostSigmoid = (int *)malloc(sizeof(int)*outputs);
  newLvl->outputs = (int *)malloc(sizeof(int)*outputs);
  newLvl->outputsDeltas = (int *)malloc(sizeof(int)*outputs);
  newLvl->previousLevel = previousLevel; 
@@ -57,22 +58,28 @@ void level_makeDecision(level *lvl) {
  } 
  //Clear all current output data
  for(i = 0; i < numberOfOutputs; i++) {
-  outputs[i] = 0;
+  outputsPreSigmoid[i] = 0;
  }
  //Run each connection
  for(i = 0; i < numberOfConnections; i++) {
-  outputsPreSigmoid[conOut[i]] += inputs[conIn[i]] * conWeight[i];
+  //printf("OutputsPreSigmoid: was:%i, conIn:%i, conOut:%i, in:%i, weight:%f\n",outputsPreSigmoid[conOut[i]],conIn[i],conOut[i],inputs[conIn[i]],conWeight[i]);
+  outputsPreSigmoid[conOut[i]] += (int)((float)inputs[conIn[i]] * conWeight[i]);
  }
  //Apply sigmoids
  for(i = 0; i < numberOfOutputs; i++ ) {
   outputs[i] = quickSigmoid_Sigmoid(outputsPreSigmoid[i]);
+  //printf("outputPreSigmoid:%i\n",outputsPreSigmoid[i]);
  }
 }
 void level_learn(level *lvl, float speed) {
  int i;
  unsigned int numberOfOutputs, numberOfInputs, numberOfConnections;
  int *outputsDeltas, *outputsPreSigmoid, *outputs, *inputs, *outputsIdealPostSigmoid;
+ int *previousOutputs,*previousOutputsIdealPostSigmoid;
+ unsigned char *conIn, *conOut;
  float *conWeight; 
+ conIn = lvl->conIn;
+ conOut = lvl->conOut;
  outputsPreSigmoid = lvl->outputsPreSigmoid;
  outputsDeltas = lvl->outputsDeltas;
  conWeight = lvl->conWeight;
@@ -90,16 +97,29 @@ void level_learn(level *lvl, float speed) {
  //Compute the delta from the ideals we've been given
  for(i=0; i < numberOfOutputs ;i++) {
   outputsDeltas[i] = quickSigmoid_inverseSigmoid(outputsIdealPostSigmoid[i]) - outputsPreSigmoid[i];
+  //printf("Delta %i,%i\n",i,outputsDeltas[i]); 
  }
  //Train the weights
  for(i=0; i < numberOfConnections ;i++) {
-  conWeight[i] += outputsDeltas[i]/inputs[i] * speed; 
+  //printf("--Running connection %i\n",i);
+  //printf("ConWeight %f\n",conWeight[i]);
+  //printf("outputsDel %i\n",outputsDeltas[conOut[i]]);
+  //printf("inputs %i\n",inputs[conIn[i]]);
+  if(inputs[conIn[i]] != 0) {
+   conWeight[i] += (float)outputsDeltas[conOut[i]]/(float)inputs[conIn[i]] * speed; 
+  }
+  //printf("newConWeight %f\n",conWeight[i]);
  } 
  //Produce the next down set of ideal outputs
  if(lvl->previousLevel != NULL) { 
-  outputsIdealPostSigmoid = lvl->previousLevel->outputsIdealPostSigmoid;
-  for(i=0; i < numberOfInputs ;i++) { 
-    outputsIdealPostSigmoid[i] += inputs[i] + outputsDeltas[i]/conWeight[i] * speed;
+  previousOutputsIdealPostSigmoid = lvl->previousLevel->outputsIdealPostSigmoid;
+  //Clear the ideals from the next set down
+  for(i=0; i < numberOfInputs ;i++) {
+    previousOutputsIdealPostSigmoid[i] = inputs[i]; 
+  }
+  //Run each connection to average this lower one 
+  for(i=0; i < numberOfConnections ;i++) {//TODO: IS THIS EVEN LOGICALLY RIGHT?? 
+    previousOutputsIdealPostSigmoid[i] += outputsDeltas[conOut[i]]/conWeight[i] * speed;
   }
  } 
 }
--- brain/level_test.c
+++ brain/level_test.c
@@ -7,6 +7,7 @@
 int level_test_makeDecision() {
  int i;
  level *lvl = level_makeNew(4,4,2,NULL);
+ printf("The decision tests\n"); 
  for(i=0; i < 4 ;i++) {
   lvl->firstInputs[i] = i*AG_INT_CONVERSION;
  }
@@ -17,9 +18,44 @@ int level_test_makeDecision() {
  level_print(lvl);
  return 1;
 }
+int level_test_learn() {
+ int i;
+ float ideal[4] = {0.0,0.0,0.0,0.5}; 
+ level *lvl = level_makeNew(4,4,2,NULL);
+ //First test: does it learn a basic thing
+ for(i=0; i < 4 ;i++) {
+  lvl->firstInputs[i] = (i+1)*AG_INT_CONVERSION;
+  lvl->outputsIdealPostSigmoid[i] = (int)(ideal[i]*AG_INT_CONVERSION);
+ }
+ lvl->conIn[0] = 0;
+ lvl->conOut[0] = 0;
+ lvl->conWeight[0] = 1.0;
+ lvl->conIn[1] = 3;
+ lvl->conOut[1] = 3;
+ lvl->conWeight[1] = 0.01;
+ for(i=0; i < 1000 ;i++) {
+  level_makeDecision(lvl);
+  level_learn(lvl,0.1);
+ }
+ if(lvl->outputs[0]/(float)AG_INT_CONVERSION < -0.01 || 
+    lvl->outputs[0]/(float)AG_INT_CONVERSION > 0.01 || 
+    lvl->outputs[3]/(float)AG_INT_CONVERSION < 0.4 || 
+    lvl->outputs[3]/(float)AG_INT_CONVERSION > 0.6) {
+  printf("Failed: Learning test 1\n"); 
+  level_print(lvl);
+  for(i=0; i < 4 ;i++) {
+   printf("%f\n",(float)lvl->outputs[i]/(float)AG_INT_CONVERSION);
+  } 
+  return 0;
+ }
+ return 1;
+}
 int level_test() {
  if (level_test_makeDecision() == 0) {
   printf("Failed: Level Decision\n");
+  return 0; }
+ if (level_test_learn() == 0) {
+  printf("Failed: Level Learn\n");
   return 0; }
  return 1;
 }
